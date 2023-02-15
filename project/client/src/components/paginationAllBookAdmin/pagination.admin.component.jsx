d80f8eacb5a74c42c40f51294e2b76fc38e1fc8e
DonnuTs69 <111718022+DonnuTs69@users.noreply.github.com>
2023-02-15 23:25:20 +0700

    pagination fix

--- project/client/src/components/paginationAllBookAdmin/pagination.admin.component.jsx
+++ project/client/src/components/paginationAllBookAdmin/pagination.admin.component.jsx
@@ -22,128 +22,161 @@ import {
 } from "@mui/material"
 import DeleteIcon from "@mui/icons-material/Delete"
 import EditIcon from "@mui/icons-material/Edit"
+import { useSearchParams } from "react-router-dom"
 
 const PaginationAdmin = () => {
-  const [currentPage, setCurrentPage] = useState(1)
-  const [pagination, setPagination] = useState({})
+  const [searchParams, setSearchParams] = useSearchParams()
   const [books, setBooks] = useState([])
   const [showTruncate, setShowTruncate] = useState(true)
+  const [currentPage, setCurrentPage] = useState(
+    parseInt(searchParams.get("page")) || 0
+  )
+  const [rowPerPage, setRowPerPage] = useState(5)
+
+  // console.log(currentPage)
+
+  const columns = [
+    { id: "title", label: "Title", minWidth: 170 },
+    { id: "author", label: "Author", minWidth: 170, align: "center" },
+    { id: "description", label: "Description", minWidth: 170, align: "center" },
+    { id: "category", label: "Category", minWidth: 170, align: "center" },
+    { id: "book images", label: "Book Images", minWidth: 170, align: "center" },
+    { id: "edit", label: "Edit", minWidth: 170, align: "center" },
+    { id: "delete", label: "Delete", minWidth: 170, align: "center" },
+  ]
 
   const truncate = (input) =>
     input.length > 40 ? `${input.substring(0, 40)}...` : input
 
+  const handleChangeRowsPerPage = (event) => {
+    // setRowPerPage(+event.target.value)
+    setCurrentPage(0)
+  }
+
+  const handlePage = (event, newPage) => {
+    setCurrentPage(newPage)
+    // console.log(newPage)
+  }
+
   const foundAllBooks = async (page = 1) => {
     try {
-      const response = await API.get(`/api/admin-books/?page=${page}&record=6`)
+      const response = await API.get(`/api/admin-books/`)
       setBooks(response.data.data)
-      setPagination(response.data.pagination)
-      console.log(response)
     } catch (err) {
       console.log(err)
     }
   }
-  console.log(books)
-  console.log(pagination)
+  // console.log(books)
+  // console.log(pagination)
 
-  const handlePage = (event, value) => {
-    event.preventDefault()
-    setCurrentPage(value)
-    foundAllBooks(value)
-  }
   useEffect(() => {
     foundAllBooks()
     //   window.localStorage.setItem("Pagination", JSON.stringify(books, pagination))
   }, [])
+
+  useEffect(() => {
+    setSearchParams({ page: currentPage })
+  }, [currentPage, setSearchParams])
+  // useEffect(() => {
+  //   window.localStorage.setItem("currentPage", JSON.stringify(currentPage))
+  // }, [currentPage])
+
+  // useEffect(() => {
+  //   const pageNow = window.localStorage.getItem("currentPage")
+  //   if (pageNow !== null) setCurrentPage(JSON.parse(pageNow))
+  // }, [])
+
   return (
     <>
-      {/* <Box display="grid" gap="10px" gridTemplateColumns="repeat(3, 1fr)">
-        {books.map((val) => (
-          <Card sx={{ maxWidth: 345, border: "1px solid red", paddingTop: 2 }}>
-            <CardMedia
-              component="img"
-              sx={{
-                height: "auto",
-                width: 180,
-                display: "block",
-                margin: "0 auto",
-              }}
-              image={val.Book_Pictures[0].book_picture}
-            />
-            <CardContent sx={{ textAlign: "center" }}>
-              <Typography variant="h4" component="div">
-                {val.title}
-              </Typography>
-              <Typography variant="h6" component="div">
-                {val.author}
-              </Typography>
-              <Typography variant="body2">
-                {showTruncate ? truncate(val.description) : val.description}
-              </Typography>
-            </CardContent>
-          </Card>
-        ))}
-      </Box>
-
-      <Pagination
-        count={pagination.totalPage}
-        page={currentPage}
-        onChange={handlePage}
-        color="primary"
-        sx={{ justifyContent: "center", display: "flex", bottom: "0" }}
-      /> */}
-
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell align="center">Author</TableCell>
-              <TableCell align="center">Description</TableCell>
-              <TableCell align="center">Books Img</TableCell>
-              <TableCell align="center">Edit</TableCell>
-              <TableCell align="center">Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {books.map((val) => (
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer>
+          <Table size="medium">
+            <TableHead>
               <TableRow>
-                <TableCell>{val.title}</TableCell>
-                <TableCell align="center">{val.author}</TableCell>
-                <TableCell align="center">
-                  {showTruncate ? truncate(val.description) : val.description}
-                </TableCell>
-                <TableCell>
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      height: "auto",
-                      width: 50,
-                      display: "block",
-                      margin: "0 auto",
-                    }}
-                    image={val.Book_Pictures[0].book_picture}
-                  />
-                </TableCell>
-                <TableCell>
-                  <IconButton>
-                    <EditIcon />
-                  </IconButton>
-                </TableCell>
-                <TableCell>
-                  <IconButton>
-                    <DeleteIcon />
-                  </IconButton>
-                </TableCell>
+                {columns.map((col) => (
+                  <TableCell
+                    key={col.id}
+                    align={col.align}
+                    style={{ minWidth: col.minWidth }}
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    {col.label}
+                  </TableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination onPageChange={handlePage} />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {books
+                .slice(
+                  currentPage * rowPerPage,
+                  currentPage * rowPerPage + rowPerPage
+                )
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.code}
+                    >
+                      {/* {columns.map((column) => {
+                      const value = row[column.id]
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      )
+                    })} */}
+                      <TableCell>{row.title}</TableCell>
+                      <TableCell align="center">{row.author}</TableCell>
+                      <TableCell align="center">
+                        {showTruncate
+                          ? truncate(row.description)
+                          : row.description}
+                      </TableCell>
+                      <TableCell align="center">
+                        {row.Category.category_name}
+                      </TableCell>
+                      <TableCell>
+                        <CardMedia
+                          component="img"
+                          sx={{
+                            height: "auto",
+                            width: 50,
+                            display: "block",
+                            margin: "0 auto",
+                          }}
+                          image={row.Book_Pictures[0].book_picture}
+                        />
+                      </TableCell>
+                      <TableCell align="center">
+                        <IconButton>
+                          <EditIcon color="info" />
+                        </IconButton>
+                      </TableCell>
+                      <TableCell align="center">
+                        <IconButton>
+                          <DeleteIcon color="error" />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  )
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5]}
+          component="div"
+          count={books.length}
+          rowsPerPage={rowPerPage}
+          page={currentPage}
+          onPageChange={handlePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
     </>
   )
 }
--- project/server/controller/admin-books.Controller.js
+++ project/server/controller/admin-books.Controller.js
@@ -5,47 +5,47 @@ module.exports = {
   getAllAdminBooks: async (req, res) => {
     try {
       // menggunakan parseInt karena limit & omset hanya menerima parameter integer
-      let limit = parseInt(req.query.record) || 5
-      let page = parseInt(req.query.page) || 1
+      // let limit = parseInt(req.query.record) || 5
+      // let page = parseInt(req.query.page) || 1
 
-      let start = 0 + (page - 1) * limit
-      let end = page * limit
+      // let start = 0 + (page - 1) * limit
+      // let end = page * limit
 
-      const adminBooks = await findAllAdminBooks.findAndCountAll({
-        include: [{ model: db.Book_Picture }],
-        limit: limit,
-        offset: start,
+      // const adminBooks = await findAllAdminBooks.findAndCountAll({
+      //   include: [{ model: db.Book_Picture }],
+      //   limit: limit,
+      //   offset: start,
 
-        // limit: 5,
-        // offset: 0,
-      })
+      //   // limit: 5,
+      //   // offset: 0,
+      // })
 
-      let countFiltered = adminBooks.count
-      let pagination = {}
-      pagination.totalRow = adminBooks.count
-      pagination.totalPage = Math.ceil(countFiltered / limit)
-      // Math.ceil digunakan ketika ada pembagian untuk pembulatan ke atas
+      // let countFiltered = adminBooks.count
+      // let pagination = {}
+      // pagination.totalRow = adminBooks.count
+      // pagination.totalPage = Math.ceil(countFiltered / limit)
+      // // Math.ceil digunakan ketika ada pembagian untuk pembulatan ke atas
 
-      // next akan muncul ketika ada data di page selanjutnya
-      if (end < countFiltered) {
-        pagination.next = {
-          page: page + 1,
-          limit,
-        }
-      }
+      // // next akan muncul ketika ada data di page selanjutnya
+      // if (end < countFiltered) {
+      //   pagination.next = {
+      //     page: page + 1,
+      //     limit,
+      //   }
+      // }
 
-      if (start > 0) {
-        pagination.prev = {
-          page: page - 1,
-          limit,
-        }
-      }
+      // if (start > 0) {
+      //   pagination.prev = {
+      //     page: page - 1,
+      //     limit,
+      //   }
+      // }
 
-      return res.status(200).json({
-        message: "Found All Books",
-        data: adminBooks.rows,
-        pagination,
-      })
+      // return res.status(200).json({
+      //   message: "Found All Books",
+      //   data: adminBooks.rows,
+      //   pagination,
+      // })
 
       // const default_page = 1
       // const default_limit = 3
@@ -68,6 +68,21 @@ module.exports = {
       //   pageCount,
       //   total: adminBooks.count,
       // })
+
+      const foundAllAdminBooks = await findAllAdminBooks.findAll({
+        include: [
+          {
+            model: db.Categories,
+          },
+          {
+            model: db.Book_Picture,
+          },
+        ],
+      })
+
+      return res.status(200).json({
+        data: foundAllAdminBooks,
+      })
     } catch (err) {
       console.log(err)
       return res.status(500).json({
