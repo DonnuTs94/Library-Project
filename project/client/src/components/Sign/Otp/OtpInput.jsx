246064779feea25e721f1abd25f5988cd976bb71
danarbastian7 <danarbastian7@gmail.com>
2023-03-03 16:47:43 +0900

    eight commit

--- project/client/src/components/Sign/Otp/OtpInput.jsx
+++ project/client/src/components/Sign/Otp/OtpInput.jsx
@@ -10,113 +10,122 @@ import {
   Button,
 } from "@mui/material"
 import SendIcon from "@mui/icons-material/Send"
+import { otpVerif } from "../../../lib/register/otpVerif"
 
 const OtpInput = () => {
+  const { formik, handleChange } = otpVerif()
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    formik.handleSubmit()
+    setShowSnackbar(true)
+  }
   return (
-    <Grid
-      sx={{
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "0 auto",
-        maxWidth: "500px",
-        // border: "2px solid red",
-      }}
-      container
-    >
+    <form onSubmit={handleFormSubmit} onChange={handleChange}>
       <Grid
         sx={{
-          display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          fontSize: { md: "30px", xs: "10px" },
-          marginBottom: "20px",
+          margin: "0 auto",
+          maxWidth: "500px",
+          // border: "2px solid red",
         }}
         container
-        spacing={{ md: 3, xs: 0.5 }}
       >
-        <Grid item width={{ md: "100px", xs: "60px" }}>
-          <OutlinedInput
-            inputProps={{
-              maxLength: 1,
-              pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
-            }}
-            style={{
-              textAlign: "center",
-              fontSize: "25px",
-              textAlignLast: "center",
-              fontWeight: "bolder",
-            }}
-          />
+        <Grid
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: { md: "30px", xs: "10px" },
+            marginBottom: "20px",
+          }}
+          container
+          spacing={{ md: 3, xs: 0.5 }}
+        >
+          <Grid item width={{ md: "100px", xs: "60px" }}>
+            <OutlinedInput
+              inputProps={{
+                maxLength: 1,
+                pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
+              }}
+              style={{
+                textAlign: "center",
+                fontSize: "25px",
+                textAlignLast: "center",
+                fontWeight: "bolder",
+              }}
+            />
+          </Grid>
+          <Grid item width={{ md: "100px", xs: "60px" }}>
+            <OutlinedInput
+              inputProps={{
+                maxLength: 1,
+                pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
+              }}
+              style={{
+                textAlign: "center",
+                fontSize: "25px",
+                textAlignLast: "center",
+                fontWeight: "bolder",
+              }}
+            />
+          </Grid>
+          <Grid item width={{ md: "100px", xs: "60px" }}>
+            <OutlinedInput
+              inputProps={{
+                maxLength: 1,
+                pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
+              }}
+              style={{
+                textAlign: "center",
+                fontSize: "25px",
+                textAlignLast: "center",
+                fontWeight: "bolder",
+              }}
+            />
+          </Grid>
+          <Grid item width={{ md: "100px", xs: "60px" }}>
+            <OutlinedInput
+              inputProps={{
+                maxLength: 1,
+                pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
+              }}
+              style={{
+                textAlign: "center",
+                fontSize: "25px",
+                textAlignLast: "center",
+                fontWeight: "bolder",
+              }}
+            />
+          </Grid>
+          <Grid item width={{ md: "100px", xs: "60px" }}>
+            <OutlinedInput
+              inputProps={{
+                maxLength: 1,
+                pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
+              }}
+              style={{
+                textAlign: "center",
+                fontSize: "25px",
+                textAlignLast: "center",
+                fontWeight: "bolder",
+              }}
+            />
+          </Grid>
         </Grid>
-        <Grid item width={{ md: "100px", xs: "60px" }}>
-          <OutlinedInput
-            inputProps={{
-              maxLength: 1,
-              pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
-            }}
-            style={{
-              textAlign: "center",
-              fontSize: "25px",
-              textAlignLast: "center",
-              fontWeight: "bolder",
-            }}
-          />
-        </Grid>
-        <Grid item width={{ md: "100px", xs: "60px" }}>
-          <OutlinedInput
-            inputProps={{
-              maxLength: 1,
-              pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
-            }}
-            style={{
-              textAlign: "center",
-              fontSize: "25px",
-              textAlignLast: "center",
-              fontWeight: "bolder",
-            }}
-          />
-        </Grid>
-        <Grid item width={{ md: "100px", xs: "60px" }}>
-          <OutlinedInput
-            inputProps={{
-              maxLength: 1,
-              pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
-            }}
-            style={{
-              textAlign: "center",
-              fontSize: "25px",
-              textAlignLast: "center",
-              fontWeight: "bolder",
-            }}
-          />
-        </Grid>
-        <Grid item width={{ md: "100px", xs: "60px" }}>
-          <OutlinedInput
-            inputProps={{
-              maxLength: 1,
-              pattern: "^[0-9]*[a-zA-Z]?[0-9]*$",
-            }}
-            style={{
-              textAlign: "center",
-              fontSize: "25px",
-              textAlignLast: "center",
-              fontWeight: "bolder",
-            }}
-          />
-        </Grid>
+        <Button
+          variant="contained"
+          sx={{
+            fontSize: { md: "20px", xs: "10px" },
+            fontWeight: "bolder",
+            borderRadius: "10px",
+          }}
+          endIcon={<SendIcon />}
+        >
+          Send
+        </Button>
       </Grid>
-      <Button
-        variant="contained"
-        sx={{
-          fontSize: { md: "20px", xs: "10px" },
-          fontWeight: "bolder",
-          borderRadius: "10px",
-        }}
-        endIcon={<SendIcon />}
-      >
-        Send
-      </Button>
-    </Grid>
+    </form>
   )
 }
 
---
+++ project/client/src/lib/register/otpVerif.js
@@ -0,0 +1,28 @@
+import { useFormik } from "formik"
+import { API } from "../../api"
+
+export const otpVerif = () => {
+  const formik = useFormik({
+    initialValues: {
+      otpInput: "",
+    },
+    onSubmit: async ({ otpInput }) => {
+      try {
+        const response = await API.post(`/auth/verify`, {
+          otpInput,
+        })
+        formik.setFieldValue({
+          otpInput: "",
+        })
+      } catch (err) {
+        return err.response
+      }
+    },
+  })
+  const handleChange = ({ target }) => {
+    const { name, value } = target
+    formik.setFieldValue(name, value)
+  }
+
+  return { formik, handleChange }
+}
