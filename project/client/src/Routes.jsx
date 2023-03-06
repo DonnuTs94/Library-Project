a7980d03bd87d4a793027921dda6b77088c1249f
RGS <nuradlifathan@gmail.com>
2023-03-06 20:09:05 +0700

    update routes

--- project/client/src/Routes.jsx
+++ project/client/src/Routes.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import Category from "./pages/admin/categories/Category"
-import AdminDashboard from "./pages/admin/Dashboard"
+// import AdminDashboard from "./pages/admin/Dashboard"
 
 const LibraryRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        {/* <Route path="/admin/dashboard" element={<AdminDashboard />} /> */}
         <Route path="/admin/categories" element={<Category />} />
       </Routes>
     </>
