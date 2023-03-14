82f743cea49d4e6273a5875db1f6236ea805aad7
RGS <nuradlifathan@gmail.com>
2023-03-14 15:26:20 +0700

    FEAT : uploader

--- project/server/routes/admin-books.Route.js
+++ project/server/routes/admin-books.Route.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const adminBookController = require("../controller/admin-books.Controller")
+const { upload } = require("../lib/uploader")
 
 const router = express.Router()
 
 router.get("/", adminBookController.getAllAdminBooks)
+// CREATE Book
+router.post("/create", upload({}), adminBookController.createBooks)
+// Delete Book
+router.delete("/delete/:id", adminBookController.deleteBooks)
 
 module.exports = router
